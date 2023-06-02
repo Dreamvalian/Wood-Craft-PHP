1fa3112b524564c21827ed33d97e660780f1ee44
Hanif Nugraha <nailavmaerd@gmail.com>
2023-06-02 20:28:51 +0700

    update register form

---
+++ woodcraft.sql
@@ -0,0 +1,162 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 02, 2023 at 01:14 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `woodcraft`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kayu`
+--
+
+CREATE TABLE `kayu` (
+  `product_id` varchar(10) NOT NULL,
+  `jenis_kayu` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kayu`
+--
+
+INSERT INTO `kayu` (`product_id`, `jenis_kayu`) VALUES
+('P01', 'Kayu Mahoni'),
+('P02', 'Kayu Jati');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemesanan`
+--
+
+CREATE TABLE `pemesanan` (
+  `order_id` varchar(10) NOT NULL,
+  `user_id` varchar(10) NOT NULL,
+  `product_id` varchar(10) DEFAULT NULL,
+  `model` varchar(20) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` double NOT NULL,
+  `grandtotal` double DEFAULT NULL,
+  `payment_status` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pemesanan`
+--
+
+INSERT INTO `pemesanan` (`order_id`, `user_id`, `product_id`, `model`, `quantity`, `price`, `grandtotal`, `payment_status`) VALUES
+('O111', 'U101', 'P02', 'Meja', 2, 5000000, 10000000, 'Unpaid');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `transaksi_id` varchar(10) NOT NULL,
+  `order_id` varchar(10) NOT NULL,
+  `user_id` varchar(10) NOT NULL,
+  `payment_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`transaksi_id`, `order_id`, `user_id`, `payment_date`) VALUES
+('T1', 'O111', 'U101', '2023-06-28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` varchar(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `email`, `password`, `tanggal_lahir`, `role`) VALUES
+('U101', 'joseph', 'joseph@gmail.com', 'apaweh', '2023-06-02', 'customer');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kayu`
+--
+ALTER TABLE `kayu`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`transaksi_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pemesanan`
+--
+ALTER TABLE `pemesanan`
+  ADD CONSTRAINT `pemesanan_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `pemesanan_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `kayu` (`product_id`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `pemesanan` (`order_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
