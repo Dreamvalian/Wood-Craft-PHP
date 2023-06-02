0cc04cf24fe724bebfca068576fe82d1aecad29b
Muhammad Hadiansyah <hadiansyah21rabbani5@gmail.com>
2023-06-02 20:18:22 +0700

    Adding login functionality

---
+++ pages/register.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Woodcraft - Register</title>
+  <link rel="stylesheet" href="../styles/pages.css" />
+</head>
+
+<body>
+  <div class="container">
+    <form class="form">
+      <h2>Register</h2>
+      <input type="text" placeholder="Username" required />
+      <input type="email" placeholder="Email" required />
+      <input type="password" placeholder="Password" required />
+      <input type="password" placeholder="Confirm Password" required />
+      <button type="submit">Register</button>
+      <p class="login-link">
+        Already have an account? <a href="login.php">Login</a>
+      </p>
+    </form>
+  </div>
+</body>
+
+</html>
---
+++ server/connection.php
@@ -0,0 +1,3 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "woodcraft")
+    or die("Can't connect to the database");
