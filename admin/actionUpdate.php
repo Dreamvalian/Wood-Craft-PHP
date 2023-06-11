088a37ed0727b717e3453ba9e1919fc99298c3e3
Muhammad Hadiansyah <hadiansyah21rabbani5@gmail.com>
2023-06-12 00:57:58 +0700

    Fixing admin, minor fix on place_order

--- admin/actionUpdate.php
+++ admin/actionUpdate.php
@@ -1,14 +1,14 @@
 <?php
-    include('../server/connection.php');
-    $path = "./assets/images" . basename($_FILES['image']['name']);
+include('../server/connection.php');
+$path = "../assets/images/" . basename($_FILES['image']['name']);
 
-    if (isset($_POST['save_btn'])) {
+if (isset($_POST['save_btn'])) {
     $product_id = $_GET['product_id'];
-    $product_name= $_POST['product_name'];
+    $product_name = $_POST['product_name'];
     $product_price = $_POST['product_price'];
     $product_description = $_POST['product_description'];
     $stok = $_POST['stok'];
-    $image = $_FILES['image']['name'];   
+    $image = $_FILES['image']['name'];
     move_uploaded_file($_FILES['image']['tmp_name'], $path);
 
     $query = "UPDATE kayu SET jenis_kayu = '$product_name',product_price='$product_price',product_description='$product_description',stok='$stok' ,product_image = '$image' WHERE product_id = '$product_id'";
@@ -20,4 +20,3 @@
 
     header('location: tables.php');
 }
-?>
